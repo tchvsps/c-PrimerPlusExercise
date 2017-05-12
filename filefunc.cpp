4b0aacf23dfb388cc308a4d3c4a48dda13bb1194
Mintong <tchvsps@outlook.com>
2017-05-12 12:51:35 +0800

    5/11 12:51

---
+++ filefunc.cpp
@@ -0,0 +1,11 @@
+// filefunc.cpp -- function with ostream & parameter
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+void file_it(ostream & os, double fo, const double fe[], int n);
+const int LIMIT=5;
+int main(){
+  ofstream fout;
+
+}
