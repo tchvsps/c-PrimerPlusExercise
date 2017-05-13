983d8f3cc642565082d6420b63d4766ebfca7506
tchvsps <tchvsps@outlook.com>
2017-05-13 11:45:19 +0800

    chapter 9 finished

---
+++ file1.cpp
@@ -0,0 +1,16 @@
+// file.cpp -- example of a three-file program
+#include <iostream>
+#include "coordin.h"
+using namespace std;
+int main(){
+  rect rplace;
+  polar pplace;
+  cout<<"Enter the x and y values: ";
+  while(cin>>rplace.x>>rplace.y){
+    pplace=rect_to_polar(rplace);
+    show_polar(pplace);
+    cout<<"Next two numbers (q to quit): ";
+  }
+  cout<<"Bye!\n";
+  return 0;
+}
