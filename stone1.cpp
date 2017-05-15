d3a05f17717821d6d6405b40da49a4a21cfa5b3c
tchvsps <tchvsps@outlook.com>
2017-05-15 09:43:00 +0800

    chapther 11 finished

---
+++ stone1.cpp
@@ -0,0 +1,15 @@
+// stone1.cpp -- user-defined conversion functions
+// compile with stonewt1.cpp
+#include <iostream>
+#include "stonewt1.h"
+
+int main(){
+  using std::cout;
+  Stonewt poppins(9,2.8);
+  double p_wt=poppins;
+  cout<<"Convert to double => ";
+  cout<<"Poppins: "<<p_wt<<" pounds.\n";
+  cout<<"Convert to int => ";
+  cout<<"Poppins: "<<int(poppins)<<" pounds.\n";
+  return 0;
+}
---
+++ usetime2.cpp
@@ -0,0 +1,38 @@
+// usetime2.cpp -- using the third raft of the Time class
+// compile usetime2.cpp and mytime2.cpp together
+#include <iostream>
+#include "mytime2.h"
+int main(){
+  using std::cout;
+  using std::endl;
+  Time weeding(4,35);
+  Time waxing(2,47);
+  Time total;
+  Time diff;
+  Time adjusted;
+
+  cout<<"weeding time = ";
+  weeding.Show();
+  cout<<endl;
+
+  cout<<"waxing time = ";
+  waxing.Show();
+  cout<<endl;
+
+  cout<<"total work time = ";
+  total=weeding + waxing;
+  total.Show();
+  cout<<endl;
+
+  diff=weeding - waxing;
+  cout<<"weeding - waxing time = ";
+  diff.Show();
+  cout<<endl;
+
+  adjusted=total*1.5;
+  cout<<"adjusted work time = ";
+  adjusted.Show();
+  cout<<endl;
+
+  return 0;
+}
